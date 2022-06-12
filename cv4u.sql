751e508fff4ea3ad474d05001e50cd235b6ac46d
Loan Samai <loansamai@gmail.com>
2022-06-12 14:53:20 +0200

    commit

---
+++ cv4u.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 12 juin 2022 à 13:20
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `cv4u`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `prenom` varchar(20) DEFAULT NULL,
+  `nom` varchar(20) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `pwd` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `prenom`, `nom`, `mail`, `pwd`) VALUES
+(1, 'Loan', 'Samai', 'loansamai@gmail.com', '$2y$10$NsOV7dykMoxgety/7JnbzOyKwXiuwrAJ9SOrxWoDkaptotOGQX3I6');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- remplirCv.php
+++ remplirCv.php
@@ -29,18 +29,24 @@ session_start();
             <div class="enligne">
                 <div class="info-perso">
                     <p>Informations personnelles</p>
-                    <input type="text" name="nom" placeholder="Votre nom" class="notfile">
-                    <input type="text" name="adresse" placeholder="Votre adresse" class="notfile">
+                    <input type="text" name="nom1" placeholder="Votre nom" class="notfile">
+                    <input type="text" name="adresse1" placeholder="Votre adresse" class="notfile">
                     <input type="text" name="mail" placeholder="Votre mail" class="notfile">
                     <input type="text" name="numtel" placeholder="Votre numéro de téléphone" class="notfile">
                     <label>Choissisez votre photo :</label>
                     <br>
                     <input type="file" name="photo">
                 </div>
-                <div class="info-perso">
+                <div class="experiencespro">
+                    <span>Veillez à séparer chaque champ d'une virgule :</span>
+                    <p>Objet du CV</p>
+                    <input type="text" name="objet" placeholder="Quel poste visez-vous ?">
                     <p>Compétences</p>
-                    <p>Veillez à séparer chaque compétence d'une virgule</p>
                     <input type="text" name="competences" placeholder="Vos compétences" class="notfile">
+                    <p>Hobbies</p>
+                    <input type="text" name="hobbies" placeholder="Vos hobbies" class="notfile">
+                    <p>Langues</p>
+                    <input type="text" name="langues" placeholder="Les langues que vous parlez" class="notfile">
                 </div>
                 <div class="experiencespro">
                     <p>Expériences professionnelles</p>
@@ -52,21 +58,50 @@ session_start();
                     <div id="exp1">
                         <p>Entreprise 1</p>
                         <input type="text" name="titre1" placeholder="Nom de l'entreprise">
-                        <input type="text" name="adresse1" placeholder="Adresse de l'entreprise">
+                        <input type="text" name="adresse2" placeholder="Adresse de l'entreprise">
+                        <input type="text" name="duree1" placeholder="Durée">
                         <input type="text" name="resume1" placeholder="Résumé de l'expérience">
                     </div>
                     <div id="exp2" style="display:none;">
                         <p>Entreprise 2</p>
                         <input type="text" name="titre2" placeholder="Nom de l'entreprise">
-                        <input type="text" name="adresse2" placeholder="Adresse de l'entreprise">
+                        <input type="text" name="adresse3" placeholder="Adresse de l'entreprise">
+                        <input type="text" name="duree2" placeholder="Durée">
                         <input type="text" name="resume2" placeholder="Résumé de l'expérience">
                     </div>
                     <div id="exp3" style="display:none;">
                         <p>Entreprise 3</p>
                         <input type="text" name="titre3" placeholder="Nom de l'entreprise">
-                        <input type="text" name="adresse3" placeholder="Adresse de l'entreprise">
+                        <input type="text" name="adresse4" placeholder="Adresse de l'entreprise">
+                        <input type="text" name="duree3" placeholder="Durée">
                         <input type="text" name="resume3" placeholder="Résumé de l'expérience">
                     </div>
+                </div>
+                <div class="info-perso">
+                    <p>Diplômes obtenus/en cours d'obtention</p>
+                    <select name="nbecole" id="nbecole" onchange="getNbEcole();">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                    <div id="ecole1">
+                        <p>École numéro 1</p>
+                        <input type="text" name="nom2" placeholder="Nom de l'école">
+                        <input type="text" name="adresse5" placeholder="Adresse de l'école">
+                        <input type="text" name="annee1" placeholder="Années d'études">
+                        <input type="text" name="diplome1" placeholder="Diplôme obtenu">
+                    </div>
+                    <div id="ecole2" style="display:none;">
+                        <p>École numéro 2</p>
+                        <input type="text" name="nom3" placeholder="Nom de l'école">
+                        <input type="text" name="adresse6" placeholder="Adresse de l'école">
+                        <input type="text" name="annee2" placeholder="Années d'études">
+                        <input type="text" name="diplome2" placeholder="Diplôme obtenu">
+                    </div>
+                </div>
+                <div class="info-perso">
+                    <p>Un projet à partager ?</p>
+                    <input type="text" name="nomprojet" placeholder="Son nom ?" class="notfile">
+                    <input type="text" name="descprojet" placeholder="En quoi consiste-t-il ?" class="notfile">
                 </div>
             </div>
             <br>
@@ -86,10 +121,23 @@ session_start();
         let nbexp = document.getElementById("nbexp").value
         if(nbexp == "2"){
             document.getElementById("exp2").style.display="block";
+            document.getElementById("exp3").style.display="none";
         }
         else if(nbexp== "3"){
             document.getElementById("exp2").style.display="block";
             document.getElementById("exp3").style.display="block";
+        }else{
+            
+            document.getElementById("exp2").style.display="none";
+            document.getElementById("exp3").style.display="none";
+        }
+    }
+    function getNbEcole(){
+        let nbecole = document.getElementById("nbecole").value
+        if(nbecole == "2"){
+            document.getElementById("ecole2").style.display="block";
+        }else{
+            document.getElementById("ecole2").style.display="none";
         }
     }
 </script>
